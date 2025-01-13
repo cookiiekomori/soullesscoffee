3e0d38998a02121c932f6b0e94baf7fb7ccec246
cookiiekomori <cookiekomori@outlook.com>
2025-01-13 13:03:22 +1000

    update

--- main.py
+++ main.py
@@ -56,11 +56,11 @@ async def status_task(bot):
             await asyncio.sleep(15)
 
 # Загрузка модулей
-for filename in os.listdir(".cogs"):
+for filename in os.listdir("cogs"):
     if filename.endswith(".py"):
         bot.load_extension("cogs." + filename[:-3])
 
-cogs = [filename[:-3] for filename in os.listdir(".cogs") if filename.endswith(".py")]
+cogs = [filename[:-3] for filename in os.listdir("cogs") if filename.endswith(".py")]
 
 @bot.slash_command(description="Выводит список доступных модулей")
 @commands.is_owner()
