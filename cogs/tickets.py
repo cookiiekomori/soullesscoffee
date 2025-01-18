69f9ecbc685a0adfd3921dfdea9f06854ab53540
cookiiekomori <cookiekomori@outlook.com>
2025-01-18 20:23:09 +1000

    update

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -5,47 +5,49 @@ def load_config():
         return json.load(f)
 
 settings = load_config()
-WEBHOOK_URL = settings['webhook_url']['command_log']  # Замените на ваш URL вебхука
+WEBHOOK_URL = settings['webhook_url']['command_log']
 
 class ReportModal(disnake.ui.Modal):
     def __init__(self, user: disnake.User):
         components = [
             disnake.ui.TextInput(
-                label="Reason for reporting",
-                placeholder="Enter the reason...",
+                label="Причина репорта",
+                placeholder="Введите причину...",
                 custom_id="report_reason",
                 style=disnake.TextInputStyle.paragraph,
                 required=True,
                 max_length=200,
             ),
         ]
-        super().__init__(title=f"Report {user}", custom_id="report_user", components=components)
-        self.user = user  # Сохраняем пользователя, на которого подается репорт
+        super().__init__(title=f"Репорт {user}", custom_id="report_user", components=components)
+        self.user = user
 
     async def callback(self, interaction: disnake.ModalInteraction):
         reason = interaction.text_values["report_reason"]
-        
-        # Создание эмбеда
+        member = self.user.mention
+
         embed = disnake.Embed(
-            title="User Report",
-            description=f"Reported {self.user.mention} for: {reason}",
-            color=disnake.Color.from_rgb(43, 45, 49)
+            title="Новый репорт",
+            color=disnake.Color.from_rgb(43, 45, 49)  # Переместил color сюда
         )
 
-        # Отправка данных о репорте через вебхук
+        embed.add_field(name="Отправил", value=interaction.user.mention, inline=True)
+        embed.add_field(name="На кого", value=f"{self.user.mention}", inline=True)
+        embed.add_field(name="Причина", value=f"```{reason}```", inline=False)
+
         async with aiohttp.ClientSession() as session:
             webhook = disnake.Webhook.from_url(WEBHOOK_URL, session=session)
             await webhook.send(embed=embed)
 
-        await interaction.response.send_message(f"Reported {self.user.mention} for: {reason}", ephemeral=True)
+        await interaction.response.send_message(f"Пользователь {self.user.mention} был репортнут за:\n{reason}", ephemeral=True)
 
 class ReportCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="report", description="Report a user")
+    @commands.slash_command(name="report", description="Репорт пользователя")
     async def report(self, interaction: disnake.ApplicationCommandInteraction, user: disnake.User):
-        """Report a user with a reason."""
+        """Отправка репорта"""
         modal = ReportModal(user)
         await interaction.response.send_modal(modal)
 
