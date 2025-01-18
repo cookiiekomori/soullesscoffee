8051d7dc71a50e2c26e5e015071a5622ad2bcc4a
cookiiekomori <cookiekomori@outlook.com>
2025-01-18 22:23:18 +1000

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -146,7 +146,7 @@ class Utilites(commands.Cog):
         await self.send_webhook(settings['webhook_url']['command_log'], embed)
         pass
 
-    @configuration.sub_command(name='Гендер роль', description='Вписать id мужской роли и женской роли')
+    @configuration.sub_command(name='gender', description='Вписать id мужской роли и женской роли')
     async def gender(self, ctx, male_role_id, female_role_id):
         df = pd.read_csv('Bases\\guild_configurations.csv')
         in_data = df[(df["Guild_id"] == ctx.guild.id) & (df["Male_role_id"] == int(male_role_id) & (df["Female_role_id"] == int(female_role_id)))]
